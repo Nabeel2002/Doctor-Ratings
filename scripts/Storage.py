fb39ac765ba975ab2cc92a2997749d02310db60a
Nabeel2002 <92419956+Nabeel2002@users.noreply.github.com>
2024-04-24 20:56:01 -0400

    script edit

--- scripts/Storage.py
+++ scripts/Storage.py
@@ -5,30 +5,36 @@ source_uri = "mongodb://c1-ds4a-2-instructor:xecatHAb5Th3VuJu8wl5@20.232.135.212
 # Target MongoDB Atlas URI
 target_uri = "mongodb+srv://Nabeel:AustinData2002@healthratings.3yebzpj.mongodb.net/?retryWrites=true&w=majority&appName=Healthratings"
 
-# Connect to the source MongoDB database
-source_client = MongoClient(source_uri)
+try:
+    # Connect to the source MongoDB database
+    source_client = MongoClient(source_uri)
 
-# Connect to the target MongoDB Atlas database
-target_client = MongoClient(target_uri)
+    # Connect to the target MongoDB Atlas database
+    target_client = MongoClient(target_uri)
 
-# Access the source database
-source_db = source_client["healthrate"]
+    # Access the source database
+    source_db = source_client["healthrate"]
 
-# Access the source collection
-source_collection = source_db["events"]
+    # Access the source collection
+    source_collection = source_db["events"]
 
-# Access the target database
-target_db = target_client["HealthRatings"]
+    # Access the target database
+    target_db = target_client["HealthRatings"]
 
-# Access or create the target collection
-target_collection = target_db["Ratings"]
+    # Access or create the target collection
+    target_collection = target_db["Ratings"]
 
-# Fetch the data from the source collection
-data = source_collection.find()
+    # Fetch the data from the source collection
+    data = list(source_collection.find())
 
-# Insert the data into the target collection
-target_collection.insert_many(data)
+    if data:
+        # Insert the data into the target collection
+        target_collection.insert_many(data)
+        print("Data successfully moved from source to target.")
+    else:
+        print("No data found in the source collection.")
 
-# Close the connections
-source_client.close()
-target_client.close()
+finally:
+    # Close the connections
+    source_client.close()
+    target_client.close()
